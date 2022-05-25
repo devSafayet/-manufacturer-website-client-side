84bf46cfa7ea977dd7945f37b03cc24be3f90f49
Safayet <mrishafayet003@gmail.com>
2022-05-26 04:19:03 +0600

    added some components and route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home/Home/Home';
+import Footer from './Components/Shared/Footer/Footer';
+import Header from './Components/Shared/Header/Header';
 
 function App() {
   return (
     <div>
-      <h2>website runnig</h2>
+      <Header />
+      <Routes>
+        <Route path='./' element={<Home />}></Route>
+      </Routes>
+
+      <Footer />
+
+
     </div>
   );
 }
