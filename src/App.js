7feaf2770c7c4019abda26d5b1f1cdbc206375c0
Safayet <mrishafayet003@gmail.com>
2022-05-27 22:11:50 +0600

    added some components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Dashboard from './Components/Dashboard/Dashboard/Dashboard';
 import Home from './Components/Home/Home/Home';
+import Blog from './Components/Pages/Blog/Blog';
+import Portfolio from './Components/Pages/Portfolio/Portfolio';
+import SignIn from './Components/Pages/SignIn/SignIn';
+import SignUp from './Components/Pages/SignUP/SignUp';
 import Footer from './Components/Shared/Footer/Footer';
 import Header from './Components/Shared/Header/Header';
 import NotFound from './Components/Shared/NotFound/NotFound';
@@ -10,7 +15,13 @@ function App() {
     <div>
       <Header />
       <Routes>
-        <Route path='./' element={<Home />}></Route>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/home' element={<Home />}></Route>
+        <Route path='/dashboard' element={<Dashboard />}></Route>
+        <Route path='/blog' element={<Blog />}></Route>
+        <Route path='/portfolio' element={<Portfolio />}></Route>
+        <Route path='/signin' element={<SignIn />}></Route>
+        <Route path='/signup' element={<SignUp />}></Route>
 
         <Route path='*' element={<NotFound />} ></Route>
       </Routes>
--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
         <div>
-            this is home
+            <Banner />
         </div>
     );
 };
