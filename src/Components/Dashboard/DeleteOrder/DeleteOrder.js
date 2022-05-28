f1e68fcfc5e88398da507036d1d6ee1d2e874b1e
Safayet <mrishafayet003@gmail.com>
2022-05-28 13:31:03 +0600

    hooks components updaded

--- src/Components/Dashboard/DeleteOrder/DeleteOrder.js
+++ src/Components/Dashboard/DeleteOrder/DeleteOrder.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 
 const DeleteOrder = () => {
+    const handledelete = ({ orderDelete, setOrderDelete }) => {
+
+        const url = `https://localhost:5000/orders/${orderDelete._id}`
+        fetch(url, {
+            method: "DELETE",
+            headers: {
+                'content-type': 'application/json',
+                'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+            },
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.deletedCount > 0) {
+                    toast.success("Delete Order successfully")
+                    setOrderDelete(null)
+                }
+            })
+    }
     return (
         <div>
-
-        </div>
+            <input type="checkbox" id="cancel-order-modal" className="modal-toggle" />
+            <div className="modal modal-bottom sm:modal-middle">
+                <div className="modal-box">
+                    <h3 className="font-bold text-lg text-red-500">Are You Sure You Want to Delete !</h3>
+                    <p className="py-4"> If you delete that not will be your order Anymore?</p>
+                    <div className="modal-action">
+                        <button className="btn btn-xs btn-error" onClick={handledelete}>Delete</button>
+                        <label for="cancel-order-modal" className="btn btn-xs">Cancel</label>
+                    </div>
+                </div>
+            </div>
+        </div >
     );
 };
 
---
+++ src/Components/Hooks/UseToken/UseToken.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+
+const UseToken = (user) => {
+    const [token, setToken] = useState(" ")
+    useEffect(() => {
+        const email = user?.user?.email;
+        const currentUser = { email: email }
+        if (email) {
+            fetch(`https://localhost:5000/user/${email}`, {
+                method: 'PUT',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(currentUser)
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log("data inserted usetoken ", data);
+                    const accesstoken = data.token;
+                    console.log(accesstoken);
+                    localStorage.setItem('accessToken', accesstoken)
+                    setToken(accesstoken)
+                })
+        }
+    }, [user])
+    return [token]
+
+};
+
+export default UseToken;
